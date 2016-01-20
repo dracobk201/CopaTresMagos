b8ba244cd1f9539be8855bd52827ed7c90c7ee14
Erick Subero <dracobk201@gmail.com>
2016-01-20 11:43:20 -0430

    Primera Subida
    -Generación del Laberinto
    -Controlador de Harry

---
+++ Assets/GenerateMaze.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateMaze : MonoBehaviour {
+
+	int n, m, x, z, cubos;
+	bool copaCreada;
+	// Use this for initialization
+	private void Awake () {
+		n = 50;
+		m = 50;
+	}
+	
+	// Update is called once per frame
+	private void Start () {
+		Crear ();
+		Debug.Log (cubos);
+		float var = n * m;
+		//if (var
+	}
+
+	private void Crear(){
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= m; j++) {
+				string name = "cube" + i+"-"+j;
+				if (Random.value <= 0.33) {
+					GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+					cube.transform.position = new Vector3 (0 + x, 0, 0 + z);
+					cube.transform.localScale = new Vector3 (1, 3, 1);
+					cube.AddComponent<BoxCollider> ();
+					cube.name = name;
+					cubos++;
+				} else if (Random.value <= 0.2 && !copaCreada && cubos>=300) {
+					GameObject copa = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+					copa.transform.position = new Vector3 (0 + x, 0, 0 + z);
+					copaCreada = true;
+				}
+				z++;
+			}
+			z = 0;
+			x++;
+		}
+	}
+}
---
+++ Assets/controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+
+	Vector3 positionActual;
+	float paso = .1f;
+	Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		positionActual = this.transform.position;
+		if (Input.GetKey(KeyCode.D)) {
+			this.transform.position = new Vector3 (positionActual.x+paso, positionActual.y, positionActual.z);
+			rb.MovePosition(this.transform.position);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			this.transform.position = new Vector3 (positionActual.x-paso, positionActual.y, positionActual.z);
+			rb.MovePosition(this.transform.position);
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			this.transform.position = new Vector3 (positionActual.x, positionActual.y, positionActual.z+paso);
+			rb.MovePosition(this.transform.position);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			this.transform.position = new Vector3 (positionActual.x, positionActual.y, positionActual.z-paso);
+			rb.MovePosition(this.transform.position);
+		}
+	}
+}
