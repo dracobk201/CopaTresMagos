ad58af2b7f7ed50b6ab83ee28bad0f5773b1e40d
Edgar Bernal <edgar.bernal@gmail.com>
2016-01-22 00:00:27 -0430

    - Algoritmo de DFS para generación del laberinto - Algoritmo de BFS para la inteligencia artificial

---
+++ Assets/Cedric.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cedric : MonoBehaviour {
+
+    
+    public GenerateMaze maze;
+
+    public Punto2D posicionCedric;
+
+
+    struct PosicionMapa
+    {
+        public Punto2D posicion;
+        public int cantidadPasos;
+        public List<Punto2D> camino;
+        public override string ToString()
+        {
+            return posicion.ToString() + "\n cantidadPasos " + cantidadPasos;
+        }
+    }
+
+    bool[,] visitados;
+
+    int[] desplazamientosX = new int[] { 1, 0, -1, 0 };
+    int[] desplazamientosY = new int[] { 0, 1, 0, -1 };
+
+    List<Punto2D> caminoACopa;
+
+    int casillaActual = -1;
+    bool moviendo;
+
+    void Start()
+    {
+        moviendo = false;
+        caminoACopa = new List<Punto2D>();
+
+        StartCoroutine(MoverCedric());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!moviendo)
+        {
+            return;
+        }
+
+
+	}
+
+    IEnumerator MoverCedric()
+    {
+        while (true)
+        {
+            if (!moviendo || caminoACopa.Count == 0)
+            {
+                yield return null; 
+                continue;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+
+            SetPosicionCedric(caminoACopa[casillaActual].x, caminoACopa[casillaActual].y);
+            casillaActual++;
+
+            if (casillaActual >= caminoACopa.Count)
+            {
+                break;
+            }
+        }
+        yield return null;
+    }
+
+    public void SetPosicionCedric(int x, int y)
+    {
+        posicionCedric.x = x;
+        posicionCedric.y = y;
+
+        transform.position = new Vector3(x, 0, y);
+    }
+
+    public void CalcularRuta(Punto2D posicion, Punto2D copa)
+    {
+        moviendo = false;
+        DoBFS(posicion.x, posicion.y, copa.x, copa.y);
+        casillaActual = 0;
+        SetPosicionCedric(posicion.x, posicion.y);
+
+        Debug.Log("longitud camino " + caminoACopa.Count);
+
+        for (int i = 0; i < caminoACopa.Count; i++)
+        {
+            Debug.Log("punto " + caminoACopa[i]);
+        }
+
+        moviendo = true;
+    }
+
+    /// <summary>
+    /// Algoritmo de búsqueda en anchura para calcular el camino más corto hacia la copa.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="posCopaX"></param>
+    /// <param name="posCopaY"></param>
+    private void DoBFS(int x, int y, int posCopaX, int posCopaY)
+    {
+        visitados = new bool[maze.n, maze.m];
+        Queue<PosicionMapa> q = new Queue<PosicionMapa>();
+
+        PosicionMapa posInicial = new PosicionMapa();
+        posInicial.posicion.x = x;
+        posInicial.posicion.y = y;
+        posInicial.cantidadPasos = 0;
+        posInicial.camino = new List<Punto2D>();
+        posInicial.camino.Add(new Punto2D(x, y));
+
+        visitados[x, y] = true;
+        q.Enqueue(posInicial);
+
+        while (q.Count > 0)
+        {
+            PosicionMapa posActual = q.Dequeue();
+            Debug.LogWarning("recorriendo pos " + posActual);
+            if (posActual.posicion.x == posCopaX && posActual.posicion.y == posCopaY)
+            {
+                //Enconttramos el camino a al copa. Guardamos la ruta.
+                caminoACopa = new List<Punto2D>(posActual.camino);
+                break;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nuevoX = posActual.posicion.x + desplazamientosX[i];
+                int nuevoY = posActual.posicion.y + desplazamientosY[i];
+
+                if (nuevoX >= 0 && nuevoX < maze.n && nuevoY >= 0 && nuevoY < maze.m)
+                {
+                    if (maze.muros[nuevoX, nuevoY] != 1 && !visitados[nuevoX, nuevoY])
+                    {
+                        visitados[nuevoX, nuevoY] = true;
+
+                        PosicionMapa nuevaPos = new PosicionMapa();
+                        nuevaPos.posicion.x = nuevoX;
+                        nuevaPos.posicion.y = nuevoY;
+                        nuevaPos.cantidadPasos = posActual.cantidadPasos + 1;
+                        nuevaPos.camino = new List<Punto2D>(posActual.camino);
+                        nuevaPos.camino.Add(nuevaPos.posicion);
+
+                        q.Enqueue(nuevaPos);
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/GenerateMaze.cs
+++ Assets/GenerateMaze.cs
@@ -2,24 +2,54 @@
 using System.Collections;
 
 public class GenerateMaze : MonoBehaviour {
+    
+    public Cedric cedric;
 
-	int n, m, x, z, cubos;
-	bool copaCreada, harry, cedric;
+	public int n, m, x, z, cubos;
+	bool copaCreada, harry, cedricBool;
 	string stringToEdit = "Hello World";
-	int[,] muros;
+
+    [HideInInspector]
+    /// <summary>
+    /// Representa el mapa. Los valores posibles son
+    /// 0 = espacio libre
+    /// 1 = pared
+    /// 2 = copa
+    /// </summary>
+    public int[,] muros;
+
+    public int posicionCopaX, posicionCopaY;
+
+    bool[,] visitados;
+
+    int[] desplazamientosX = new int[] { 1, 0, -1, 0 };
+    int[] desplazamientosY = new int[] { 0, 1, 0, -1 };
+
+    public GameObject copaGameObject;
+
 	// Use this for initialization
 	private void Awake () {
-		n = 50;
+        n = 50;
 		m = 50;
-		muros = new int[n+1, m+1]; 
+        muros = new int[n + 1, m + 1];
+        visitados = new bool[n, m];
 	}
 	
 	// Update is called once per frame
 	private void Start () {
-		Crear ();
+		//Crear ();
+        Crear2();
 		Debug.Log (cubos);
 		//float var = n * m;
 		//if (var
+
+        Punto2D posicionCedric = getPosicionAleatoriayLibre();
+        Debug.Log(" posicionCedric " + posicionCedric);
+        Punto2D posicionCopa = getPosicionAleatoriayLibre();
+        Debug.Log(" posicionCopa " + posicionCopa);
+
+        copaGameObject.transform.position = new Vector3(posicionCopa.x, 0, posicionCopa.y);
+        cedric.CalcularRuta(posicionCedric, posicionCopa);
 	}
 
 	void OnGUI(){
@@ -29,6 +59,7 @@ public class GenerateMaze : MonoBehaviour {
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= m; j++) {
 				string name = "cube" + i+"-"+j;
+                muros [i, j] = 0;
 				if (Random.value <= 0.33) {
 					GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 					cube.transform.position = new Vector3 (0 + x, 0, 0 + z);
@@ -42,18 +73,143 @@ public class GenerateMaze : MonoBehaviour {
 				
 				} else if (Random.value >= 0.8 && !harry) {
 
-				} else if (Random.value <= 0.2 && !copaCreada && cubos >= 300) {			//Esa condición de los 300, no va funcionar
-					GameObject copa = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-					copa.transform.position = new Vector3 (0 + x, 0, 0 + z);
-					copa.AddComponent<SphereCollider> ();
-					copaCreada = true;
-					muros [i, j] = 2;
-				}
-				muros [i, j] = 0;
+                } /*else if (Random.value <= 0.2 && !copaCreada && cubos >= 300) {			//Esa condición de los 300, no va funcionar
+                    GameObject copa = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+                    copa.transform.position = new Vector3 (0 + x, 0, 0 + z);
+                    copa.AddComponent<SphereCollider> ();
+                    copaCreada = true;
+                    muros [i, j] = 2;
+                }*/
+				
 				z++;
 			}
 			z = 0;
 			x++;
 		}
 	}
+
+    /// <summary>
+    /// Obtiene una posicion dentro del mapa de forma aleatoria.
+    /// </summary>
+    /// <returns>Una posición libre.</returns>
+    Punto2D getPosicionAleatoriayLibre()
+    {
+        Punto2D punto = new Punto2D();
+
+        punto.x = Random.Range(0, n);
+        punto.y = Random.Range(0, m);
+        while (muros[punto.x, punto.y] == 1)
+        {
+            punto.x = Random.Range(0, n);
+            punto.y = Random.Range(0, m);
+        }
+
+        return punto;
+    }
+
+    /// <summary>
+    /// Devuelve una permutación de direcciones a partir de 4
+    /// coordenadas (Este, Sur, Oeste, Norte).
+    /// </summary>
+    /// <returns></returns>
+    int[] permutarDirecciones()
+    {
+        int[] direcciones = new int[] { 0, 1, 2, 3 };
+
+        for (int i = 0; i < 3; i++)
+        {
+            int temp = direcciones[i];
+            int numeroRandom = Random.Range(0, 4);
+
+            //Hacer un shuffle.
+            direcciones[i] = direcciones[numeroRandom];
+            direcciones[numeroRandom] = temp;
+        }
+
+        return direcciones;
+    }
+
+
+    private void Crear2()
+    {
+        // Se inicializa el mapa.
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                visitados[i, j] = false;
+                muros[i, j] = 1;
+            }
+        }
+        //Se genera el laberinto a partir de la posicion 0, 0
+        DoDFS(0, 0);
+        x = 0;
+        z = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                string name = "cube" + i + "-" + j;
+                if (muros[i, j] == 1) // Si es una pared añade un cubo.
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(0 + x, 0, 0 + z);
+                    cube.transform.localScale = new Vector3(1, 3, 1);
+                    cube.AddComponent<BoxCollider>();
+                    cube.name = name;
+                    cubos++;
+                    //Debug.Log(i + " " + j);
+                }
+                z++;
+            }
+
+            z = 0;
+            x++;
+        }        
+    }
+
+    private void DoDFS(int x, int y)
+    {
+        int n2 = n;
+        int m2 = m;
+        int[] direcciones = permutarDirecciones();
+
+        for (int i = 0; i < direcciones.Length; i++)
+        {
+            int nuevoX = x + desplazamientosX[direcciones[i]];
+            int nuevoY = y + desplazamientosY[direcciones[i]];
+            int nuevoX_2 = x + desplazamientosX[direcciones[i]] * 2;
+            int nuevoY_2 = y + desplazamientosY[direcciones[i]] * 2;
+
+            //Aca se verifica que la nueva posicion sea válida (se encuentre dentro de los límites del mapa).
+            if (nuevoX >= 0 && nuevoX < n2 && nuevoY >= 0 && nuevoY < m2 &&
+                nuevoX_2 >= 0 && nuevoX_2 < n2 && nuevoY_2 >= 0 && nuevoY_2 < m2)
+
+            {
+                if (!visitados[nuevoX_2, nuevoY_2])
+                {
+                    muros[nuevoX, nuevoY] = 0;
+                    muros[nuevoX_2, nuevoY_2] = 0;
+
+                    visitados[nuevoX_2, nuevoY_2] = true;
+                    DoDFS(nuevoX_2, nuevoY_2);
+                }
+            }
+        }
+    }
+}
+
+public struct Punto2D
+{
+    public int x, y;
+    public Punto2D(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+    public override string ToString()
+    {
+        return "x: " + x + "; y: " + y;
+    }
 }
